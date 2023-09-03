3503d49a976719277252d3b09a8983928bcc6b7e
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-09-03 21:46:46 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.min.css';
+pixabay.defaults.headers.common['x-api-key'] =
+  '39231983-3a5a24849d135b109e286581a';
+
+async function fetchUser() {
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/name/Ukraine');
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+fetchUser();
 
 function renderGallery(cards) {
   console.log(cards.hits);
