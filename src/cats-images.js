42e9af0459570ca34a25f74148cbd952696f5c7f
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-09-07 17:00:29 +0200

    cats-images

---
+++ src/cats-images.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  '39231983-3a5a24849d135b109e286581a';
+
+const key = '39231983-3a5a24849d135b109e286581a';
+const q = '';
+const image_type = 'photo';
+const orientation = 'horizontal';
+const safesearch = 'true';
