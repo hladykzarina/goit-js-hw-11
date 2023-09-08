f278cfbc151bfed849b0bae800e70032db51620c
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-09-08 16:18:19 +0200

    cats-imgs and styles

--- src/cats-images.js
+++ src/cats-images.js
@@ -1,9 +1,53 @@
+import { gallery } from './index';
 import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  '39231983-3a5a24849d135b109e286581a';
-
-const key = '39231983-3a5a24849d135b109e286581a';
-const q = '';
-const image_type = 'photo';
-const orientation = 'horizontal';
-const safesearch = 'true';
+const API_KEY = '39231983-3a5a24849d135b109e286581a';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const limitPage = 40;
+const getImg = async (searchImage, page) => {
+  const { data } = await axios({
+    params: {
+      key: API_KEY,
+      q: searchImage,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: limitPage,
+      page: page,
+    },
+  });
+  return data;
+};
+function createMarkup(data) {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return ` 
+         <div class="photo-card">
+         <div class="img-box">
+         <a class="gallery-link" href="${largeImageURL}">
+         <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img"/> 
+         </a>
+         </div>
+         
+         <ul class="info">
+         <li class="info-item">Likes<p>${likes}</p></li>
+         <li class="info-item">Views<p>${views}</p></li>
+         <li class="info-item">Comments<p>${comments}</p></li>
+         <li class="info-item">Downloads<p>${downloads}</p></li>
+         </ul>
+         </div>
+        `;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+export { createMarkup, getImg, limitPage };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import { createMarkup, getImg, limitPage } from './cats-images';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-
+/*
 Notiflix.Notify.init({
   width: '480px',
   position: 'center-center',
@@ -10,119 +10,65 @@ Notiflix.Notify.init({
   opacity: 0.7,
   fontSize: '18px',
   cssAnimationStyle: 'zoom',
-});
-
-const galleryLightBox = new SimpleLightbox('.gallery a');
+});*/
 const refs = {
   formEl: document.querySelector('.search-form'),
-  btnEl: document.querySelector('button'),
   galleryEl: document.querySelector('.gallery'),
   loadEl: document.querySelector('.load-more'),
 };
-refs.loadEl.classList.add('hidden');
-refs.formEl.addEventListener('submit', handlerImg);
-const BASE_URL = 'https://pixabay.com/';
-const END_POINT = 'api/';
-const API_KEY = 'key=39231983-3a5a24849d135b109e286581a';
+const gallery = refs.galleryEl;
+const galleryLightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+//refs.loadEl.classList.add('hidden');
+refs.formEl.addEventListener('submit', onSubmitForm);
+refs.loadEl.addEventListener('click', onLoad);
 let page;
-let searchImage;
-function handlerImg(evt) {
+let searchImage = '';
+function onSubmitForm(evt) {
   evt.preventDefault();
-  refs.loadEl.classList.remove('hidden');
-  searchImage = refs.formEl.searchQuery.value;
-
-  if (searchImage === '') {
-    refs.loadEl.classList.add('hidden');
-    Notiflix.Notify.warning(
-      'Sorry, the input field cannot be empty. Please try again.'
-    );
-    refs.galleryEl.innerHTML = '';
+  const query = evt.currentTarget.elements.searchQuery.value;
+  if (!query.trim() || query === searchImage) {
     return;
   }
-  page = 1;
-  getImg(searchImage, page)
-    .then(response => {
-      const totalHits = response.data.totalHits;
-      const nameImage = response.data.hits;
-      if (nameImage.length === 0) {
-        refs.loadEl.classList.add('hidden');
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
-      createMarkup(response.data.hits);
-      galleryLightBox.refresh();
-    })
-    .catch(err => console.log(err));
+  searchImage = query;
   refs.galleryEl.innerHTML = '';
+  page = 1;
+  refs.loadEl.classList.add('hidden');
+  handlerImg(searchImage, page);
+  refs.formEl.reset();
 }
-refs.loadEl.addEventListener('click', onLoad);
+const handlerImg = async (searchImage, page) => {
+  try {
+    const data = await getImg(searchImage, page);
+    if (!data.hits.length) {
+      Notiflix.Notify.failure(
+        'Sorry, the input field cannot be empty. Please try again.'
+      );
+      return;
+    }
+    createMarkup(data);
+    galleryLightBox.refresh();
+    if (page === 1) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+    if (data.totalHits >= page * limitPage) {
+      refs.loadEl.classList.remove('hidden');
+    }
+    if (data.totalHits <= page * limitPage) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Oops! Something went wrong!');
+  }
+};
 function onLoad() {
+  refs.loadEl.classList.add('hidden');
   page += 1;
-  getImg(searchImage, page)
-    .then(response => {
-      const totalHits = response.data.totalHits;
-      const totalPage = totalHits / 40;
-
-      if (page > totalPage) {
-        refs.loadEl.classList.add('hidden');
-        Notiflix.Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-      createMarkup(response.data.hits);
-      galleryLightBox.refresh();
-    })
-    .catch(err => console.log(err));
+  handlerImg(searchImage, page);
 }
-async function getImg(inp, page) {
-  const response = await axios.get(
-    `${BASE_URL}${END_POINT}?${API_KEY}&q=${inp}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-  );
-  return response;
-}
-function createMarkup(arr) {
-  const markup = arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<div class="photo-card">
-    <a href="${largeImageURL}" class="js-link"><img src="${webformatURL}" alt="${tags}" width=300 loading="lazy" /></a>
-  <div class="info">
-  <div class="info-desc"><p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>${likes}</b>
-    </p></div>
-    <div class="info-desc"><p class="info-item">
-      <b>Views</b>
-    </p><p class="info-item">
-      <b>${views}</b>
-    </p>
-    </div>
-    <div class="info-desc"><p class="info-item">
-      <b>Comments</b>
-    </p><p class="info-item">
-      <b>${comments}</b>
-    </p>
-    </div>
-    <div class="info-desc"><p class="info-item">
-      <b>Downloads</b>
-    </p><p class="info-item">
-      <b>${downloads}</b>
-    </p></div>
-  </div>
-</div>`
-    )
-    .join('');
-  return refs.galleryEl.insertAdjacentHTML('beforeend', markup);
-}
+export { gallery };
